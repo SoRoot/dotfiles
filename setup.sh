a725ed79519715b95498450cda438a6b9ab9e885
SoRoot <lukas.ungerland@gmail.com>
2021-12-10 18:54:10 -0600

    added auto install of Exuberant Ctags

--- setup.sh
+++ setup.sh
@@ -4,6 +4,24 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
+#this lib must be installed to use tagbar explorer to show variables and methods
+function installCflags() {
+	tar -xvzf ctags-5.8.tar.gz
+	cd ctags-5.8
+	echo -e "\n"
+	echo "--------------Exuberant Ctags--------------"
+	echo "Please insert desired directory for Exuberant Ctags!" 
+	echo "For example /home/<USER>/dotfiles as it is predefined in the .vimrc line:98" 
+	read -p "Ctags dir: " ctagdir
+	./configure --prefix=$ctagdir
+	make
+	make check
+	make install
+	make clean
+	make distclean
+	cd ..
+}
+
 function doIt() {
 	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -18,6 +36,15 @@ function doIt() {
 	ln -svf "$(pwd)/.config/nvim" $HOME/.config
 	ln -svf "$(pwd)/.config/coc" $HOME/.config
 	ln -svf "$(pwd)/.config/htop" $HOME/.config
+	
+
+	echo -e "\n"
+	read -p "Do you want to install Exuberant Ctags? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		installCflags;
+	fi;
+
 	chsh -s $(which zsh)
 	zsh
 }
